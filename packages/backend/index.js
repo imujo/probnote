e6f400c70c9f9654a44b6ab6bb256d93d4c0a63d
imujo <ivo.mujo.3@gmail.com>
2023-08-17 14:57:13 +0200

    Configure express to work on server

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3001;
+
+const port = process.env.PORT || 3002;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
 });
