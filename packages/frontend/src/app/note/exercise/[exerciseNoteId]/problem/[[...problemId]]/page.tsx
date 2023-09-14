8fc6308b67a928597239989e69fd677e7cbaa7eb
imujo <ivo.mujo.3@gmail.com>
2023-09-14 07:50:26 +0200

    Add excalidraw canvas

--- packages/frontend/src/app/note/exercise/[exerciseNoteId]/problem/[[...problemId]]/page.tsx
+++ packages/frontend/src/app/note/exercise/[exerciseNoteId]/problem/[[...problemId]]/page.tsx
@@ -4,6 +4,18 @@ import React, { FC } from "react";
 import useGetProblem from "api/problem/hooks/useGetProblem";
 import useProblemId from "hooks/useProblemId";
 import ProblemImage from "./components/ProblemImage";
+import {
+  Excalidraw,
+  exportToCanvas,
+  exportToSvg,
+  exportToBlob,
+} from "@excalidraw/excalidraw";
+import type { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
+import {
+  AppState,
+  ExcalidrawImperativeAPI,
+  ExcalidrawProps,
+} from "@excalidraw/excalidraw/types/types";
 
 interface ProblemPageProps {}
 
@@ -13,11 +25,18 @@ const ProblemPage: FC<ProblemPageProps> = ({}) => {
   const query = useGetProblem(problemId);
 
   return (
-    <div>
+    <div className="h-[100svh]">
       <div className="box-border flex h-64 w-full justify-center border-b-[1px] border-zinc-300 p-2">
         <div className="relative h-full w-full max-w-[80%] ">
           <ProblemImage query={query} />
         </div>
+      </div>
+      <div className="h-full w-full">
+        <Excalidraw
+          onChange={(elements: readonly ExcalidrawElement[], state: AppState) =>
+            console.log("Elements :", elements, "State : ", state)
+          }
+        />
       </div>
     </div>
   );
