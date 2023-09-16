d9c6cb7b5114f6c53f2b4d89d65a02e78979c3b8
imujo <ivo.mujo.3@gmail.com>
2023-09-16 19:19:25 +0200

    remove lint checking on deployment

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "probnote.6df701e1ee630e37e32b63c2c0c136d0.r2.cloudflarestorage.com",
